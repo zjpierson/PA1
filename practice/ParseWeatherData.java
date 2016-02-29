f204080d746a335ed5d4d465a4efaeedfd7c8894
zjpierson <zachary.pierson@mines.sdsmt.edu>
2016-02-29 14:40:24 -0700

    added some more to ParseWeatherData

--- practice/ParseWeatherData.java
+++ practice/ParseWeatherData.java
@@ -17,7 +17,7 @@ JMW 160205
 
 import org.jdom2.*;
 import org.jdom2.input.SAXBuilder;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.ArrayList;
 import java.text.*;
@@ -25,67 +25,130 @@ import java.text.*;
 public class ParseWeatherData
 {
     public WeatherList WeatherData;
+    private ArrayList<String> _DataFiles;
+    private String _DataPath;
 
     //Constructor
-    public ParseWeatherData( )
+    public ParseWeatherData(String dataDirectory)
     {
-        this("2010-01.xml");
-    }
+        _DataPath = dataDirectory;
+        _DataFiles = new ArrayList<String>();
 
-    public ParseWeatherData( String filename )
-    {
-        WeatherData = new WeatherList();
-        ParseFile(filename);
-    }
+        File[] files = new File(dataDirectory).listFiles();
+        //If this pathname does not denote a directory, then listFiles() returns null. 
 
-    public void ParseFile( String filename)
-    {
-        // read and parse XML document
-        SAXBuilder builder = new SAXBuilder();
-        try
+        for (File file : files)
         {
-            Document doc = builder.build( filename );    // parse XML tags
-            Element root = doc.getRootElement();        // get root of XML tree
-            FillWeatherData( root, 0 );                    // print info in XML tree
-        }
-        // JDOMException indicates a well-formedness error
-        catch ( JDOMException e )
-        {
-            System.out.println( filename + " is not well-formed." );
-            System.out.println( e.getMessage() );
-        }
-        catch ( IOException e )
-        {
-            System.out.println( e );
+            if (file.isFile() && file.getName().contains(".xml")) 
+            {
+                _DataFiles.add(file.getName());
+            }
         }
     }
 
-    public void FillWeatherData( Element current, int depth)
+    public ParseWeatherData()
+    {
+        this("./data/");
+    }
+
+    public boolean GetDay(Date day)
+    {
+        //figure out which files to use
+        ArrayList<String> files = FindFiles(day, day);
+
+        //Parse files for the day and updates the WeatherData
+        ParseFile(files, day, day);
+
+        return true;
+    }
+
+    private ArrayList<String> FindFiles(Date startDate, Date endDate)
+    {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM");
+        ArrayList<String> fileList = new ArrayList<String>();
+
+        for(int i = 0; i < _DataFiles.size(); i++)
+        {
+            if(_DataFiles.get(i).contains(format.format(startDate)))
+            {
+                System.out.println(_DataFiles.get(i));
+                fileList.add(_DataFiles.get(i));
+            }
+        }
+        
+        return fileList;
+    }
+
+
+    public boolean ParseFile(ArrayList<String> files, Date startDate, Date endDate)
+    {
+        boolean success = false;
+
+        for(int i = 0; i < files.size(); i++)
+        {
+            
+            // read and parse XML document
+            SAXBuilder builder = new SAXBuilder();
+            try
+            {
+                Document doc = builder.build( _DataPath + files.get(i) );    // parse XML tags
+                Element root = doc.getRootElement();        // get root of XML tree
+                success = FillWeatherData( root, startDate );                    // fill WeatherData
+            }
+            // JDOMException indicates a well-formedness error
+            catch ( JDOMException e )
+            {
+                System.out.println( _DataPath + files.get(i) + " is not well-formed." );
+                System.out.println( e.getMessage() );
+            }
+            catch ( IOException e )
+            {
+                System.out.println( e );
+            }
+        }
+
+        return success;
+    }
+
+    public boolean FillWeatherData( Element current, Date startDate )
     {
         // get children of current node
         List children = current.getChildren();
         Iterator iterator = children.iterator();
+        boolean success = false;
 
         // recursively process each child node
         while ( iterator.hasNext() )
         {
             WeatherConverter dataPoint = new WeatherConverter();
             Element child = ( Element ) iterator.next();
-            FillDataPoint( child, depth + 1, dataPoint);
-            WeatherData.add(dataPoint.Convert());
+            if(FillDataPoint( child, dataPoint, startDate))
+            {
+                WeatherData.add(dataPoint.Convert());
+                success = true;
+            }
         }
+
+        return success;
     }
 
     // print XML tags and leaf node values
-    public void FillDataPoint( Element current, int depth, WeatherConverter dataPoint )
+    public boolean FillDataPoint( Element current, WeatherConverter dataPoint, Date startDate )
     {
         // get children of current node
         List children = current.getChildren();
         Iterator iterator = children.iterator();
         String name = current.getName();
 
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yy");
+
         if (name == "date")
         {
+            if(current.getValue() != format.format(startDate))
+            {
+                return false;
+            }
+
             dataPoint.date = current.getValue();
         }
         else if (name == "time")
@@ -141,7 +204,148 @@ public class ParseWeatherData
         while ( iterator.hasNext() )
         {
             Element child = ( Element ) iterator.next();
-            FillDataPoint( child, depth + 1, dataPoint );
+            if( !FillDataPoint( child, dataPoint, startDate ) )
+            {
+                return false;
+            }
         }
+
+        return true;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------------------//
+//-----------------------------------------------------------------------//
+//-----------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//    public void ParseFile( String filename)
+//    {
+//        // read and parse XML document
+//        SAXBuilder builder = new SAXBuilder();
+//        try
+//        {
+//            Document doc = builder.build( filename );    // parse XML tags
+//            Element root = doc.getRootElement();        // get root of XML tree
+//            FillWeatherData( root );                    // fill WeatherData
+//        }
+//        // JDOMException indicates a well-formedness error
+//        catch ( JDOMException e )
+//        {
+//            System.out.println( filename + " is not well-formed." );
+//            System.out.println( e.getMessage() );
+//        }
+//        catch ( IOException e )
+//        {
+//            System.out.println( e );
+//        }
+//    }
+//
+//    public void FillWeatherData( Element current )
+//    {
+//        // get children of current node
+//        List children = current.getChildren();
+//        Iterator iterator = children.iterator();
+//
+//        // recursively process each child node
+//        while ( iterator.hasNext() )
+//        {
+//            WeatherConverter dataPoint = new WeatherConverter();
+//            Element child = ( Element ) iterator.next();
+//            FillDataPoint( child, dataPoint);
+//            WeatherData.add(dataPoint.Convert());
+//        }
+//    }
+//
+//    // print XML tags and leaf node values
+//    public void FillDataPoint( Element current, WeatherConverter dataPoint )
+//    {
+//        // get children of current node
+//        List children = current.getChildren();
+//        Iterator iterator = children.iterator();
+//        String name = current.getName();
+//
+//        if (name == "date")
+//        {
+//            dataPoint.date = current.getValue();
+//        }
+//        else if (name == "time")
+//        {
+//            dataPoint.time = current.getValue();
+//        }
+//        else if (name == "temperature")
+//        {
+//            dataPoint.temperature = current.getValue();
+//        }
+//        else if (name == "humidity")
+//        {
+//            dataPoint.humidity = current.getValue();
+//        }
+//        else if (name == "barometer")
+//        {
+//            dataPoint.barometer = current.getValue();
+//        }
+//        else if (name == "windspeed")
+//        {
+//            dataPoint.windspeed = current.getValue();
+//        }
+//        else if (name == "winddirection")
+//        {
+//            dataPoint.winddirection = current.getValue();
+//        }
+//        else if (name == "windgust")
+//        {
+//            dataPoint.windgust = current.getValue();
+//        }
+//        else if (name == "windchill")
+//        {
+//            dataPoint.windchill = current.getValue();
+//        }
+//        else if (name == "heatindex")
+//        {
+//            dataPoint.heatindex = current.getValue();
+//        }
+//        else if (name == "uvindex")
+//        {
+//            dataPoint.uvindex = current.getValue();
+//        }
+//        else if (name == "rainfall")
+//        {
+//            dataPoint.rainfall = current.getValue();
+//        }
+//        else if (!iterator.hasNext())
+//        {
+//            System.out.println(name + " Not Found");
+//        }
+//
+//        // recursively process each child node
+//        while ( iterator.hasNext() )
+//        {
+//            Element child = ( Element ) iterator.next();
+//            FillDataPoint( child, depth + 1, dataPoint );
+//        }
+//    }
+//}
---
+++ practice/TempGraph.java
@@ -0,0 +1,53 @@
+/*
+                    ***** TempGraph.java *****
+
+Author: Zachary Pierson
+Class: CSC468 GUI Programming, Spring 2016
+
+Modifications:
+*/
+
+import java.util.*;
+import java.util.ArrayList;
+import java.text.*;
+
+public class TempGraph
+{
+    public TempGraph()
+    {
+        Run();
+    }
+
+    public void Run()
+    {
+        ParseWeatherData dataReader = new ParseWeatherData();
+        Calendar cal = Calendar.getInstance();
+        cal.set(2013, 0, 23);
+
+        if(!dataReader.GetDay(cal.getTime()))
+        {
+            System.out.println("GetDay failed");
+            return;
+        }
+
+//        WeatherList data = new WeatherList(dataReader.WeatherData);
+//
+//        if( data == null)
+//        {
+//            System.out.println("data is null!");
+//            return;
+//        }
+
+
+        for(int i = 0; i < data.size(); i++)
+        {
+            System.out.println(data.get(i));
+        }
+    }
+
+    public static void main( String [] args )
+    {
+        new TempGraph();
+    }
+
+}
