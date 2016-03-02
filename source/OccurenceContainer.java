c541df3ba0de61021fe640122e5b57706fe310e8
Miller <7182112@linux03.mcs.sdsmt.edu>
2016-03-01 20:48:32 -0700

    more commenting...

--- source/OccurenceContainer.java
+++ source/OccurenceContainer.java
@@ -1,7 +1,22 @@
+/*
+ * OccurenceContainer.java
+ *
+ * The occurence container was designed to hold a double as a specific value,
+ * along with the corresponding date of the occurence.
+ *
+ * @author Samantha Krantz
+ * @version CSC468 GUI Programming, Spring 2016
+ *
+ * Modifications:
+ *  Feb. 23, 2016   Created OccurenceContainer.java
+ */
 import java.util.*;
 
+/*
+ * OccurenceContainer class
+ */
 public class OccurenceContainer
 {
-  public Double dataPoint;
-  public Date date;
+  public Double dataPoint; // Double for the data being stored
+  public Date date; // Date object for the date of the occurence
 }
